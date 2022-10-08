b32cc9a9a3fcaaa004e4c0683d04098cbc55001d
mohankriz <mohankrizz45@gmail.com>
2022-10-09 01:29:42 +0530

    demo

---
+++ World_Population/src/org/world/population/Birth2_Monitering.java
@@ -0,0 +1,10 @@
+package org.world.population;
+
+public class Birth2_Monitering {
+public static void main(String[] args) {
+	Birth_Monitering data = new Birth_Monitering();
+	data.China();
+	data.France();
+	data.Japan();
+}
+}
---
+++ World_Population/src/org/world/population/Birth_Monitering.java
@@ -0,0 +1,39 @@
+package org.world.population;
+
+
+public class Birth_Monitering {
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+Birth_Monitering data = new Birth_Monitering();
+System.out.println("hello");
+    data.India();
+    data.China();
+    data.France();
+    data.Japan();
+} 
+
+private void India() {
+	// TODO Auto-generated method stub
+System.out.println("India= 4.8M");
+System.out.println("MALE= 2.4M");
+System.out.println("FEMALE=2.4M");
+}
+public void Japan() {
+	// TODO Auto-generated method stub
+	System.out.println("India= 3.8M");
+	System.out.println("MALE= 1.4M");
+	System.out.println("FEMALE=2.4M");
+}
+protected void France() {
+	// TODO Auto-generated method stub
+	System.out.println("India= 2.8M");
+	System.out.println("MALE= 0.8M");
+	System.out.println("FEMALE=2.0M");
+}
+void China() {
+	// TODO Auto-generated method stub
+	System.out.println("India= 10.2M");
+	System.out.println("MALE= 4.0M");
+	System.out.println("FEMALE=6.2M");
+}
+}
